e07f76683f2b86082f9972c96f4fba57571a120c
Charlie Hart <cehart90@gmail.com>
2021-06-10 18:02:31 -0700

    question and answer index now populating correctly. Timer working properly.

--- script.js
+++ script.js
@@ -2,64 +2,60 @@
 const startBtn = document.querySelector("#start-btn");
 const nextBtn = document.querySelector("#next-btn");
 
-//Question and answers
-const quizQuestions = document.querySelector("#quizQuestions");
-let questionEl = document.querySelector("#question");
-const answerChoices = document.querySelector("#button-choices");
-const buttonSelected = document.querySelector(".btn-options");
+let score = 0;
 
 //Question variables
 const totalQuestions = 3;
 let correctAnswer = true;
-let questionIndex
+let questionIndex = 0;
 
 //Timer variables
 const timeEl = document.querySelector(".timeLeft");
 const timesUp = document.querySelector("#timesUp");
-let secondsLeft = 90;
-
-let score = 0;
-
+let secondsLeft = 150;
 
 const questions = [
   {
-    question: "How many tutor sessions can a full-time UoFA bootcamp student get per week?",
-    correctAnswer: 2,
+    question:
+      "How many tutor sessions can a full-time UoFA bootcamp student get per week?",
+    correctAnswer: "2",
     answerOptions: ["0", "1", "2", "3"],
   },
 
   {
     question: "How much wood can a wood chuck, chuck?",
-    correctAnswer: 2,
-    answerOptions: ["Does the woodchuck care?", "Umm", "Depends", "Inconclusive"],
+    correctAnswer: "Depends",
+    answerOptions: [
+      "Does the woodchuck care?",
+      "Umm",
+      "Depends",
+      "Inconclusive",
+    ],
   },
 
   {
     question: "What is the best soda flavor?",
-    correctAnswer: 2,
+    correctAnswer: "Dr. Pepper",
     answerOptions: ["Pepsi", "Sprite", "Dr. Pepper", "SevenUp"],
   },
 ];
 
 // WHEN I click the start button
 // THEN a timer starts and I am presented with a question
-startBtn.addEventListener("click", function (e) {
+startBtn.addEventListener("click", beginQuiz);
+//Once quiz starts, question with corresponding options populates
+//If answered correctly && time/questions remain, next question
+//else if subtract 10s && time/questions remain then present next question
+//else goto score and record initials & score
+//click clear score or try again
+//newQuestion function to show hidden/vis once answer selected?
+function beginQuiz() {
   startBtn.classList.add("hidden");
-  questionIndex = 0
-  quizQuestions.classList.remove("hidden");
-  // nextQuestion();
+  $("#quizQuestions").removeClass("hidden");
+  // .classList.remove("hidden")
   setTime();
-
-// buttonSelected.addEventListener("click", function (e) {
-
-// }
-  //Once quiz starts, question with corresponding options populates
-  //If answered correctly && time/questions remain, next question
-  //else if subtract 10s && time/questions remain then present next question
-  //else goto score and record initials & score
-  //click clear score or try again
-  //newQuestion function to show hidden/vis once answer selected?
-});
+  nextQuestion();
+}
 
 function setTime() {
   const timerInterval = setInterval(function () {
@@ -79,46 +75,43 @@ function setTime() {
 //guard cluases exit functions early
 //local storage to store high scores from quiz
 //localStorage.setItem("Label", JSON.stringify(placeholder score
-//array of questions with each question being an obj.
 
 // WHEN I answer a question
 // THEN I am presented with another question
-// debugger
-// function nextQuestion(question){
-// questionEl.innerHTML = question.question;
-// }
 
-questions.forEach(question => {
-  questionEl.innerHTML = question.question;
-});
-
-// questions.forEach(answerOptions =>{
-//   const button = document.createElement('button')
-//   button.innerHTML = answerOptions.textContent
-//   button.classList.add('btn')
-//   answerChoices.innerHTML = answerOptions.answerOptions
-//   answerOptions.appendChild(button)
-// })
-// // WHEN I answer a question incorrectly
-// // THEN time is subtracted from the clock
-// function wrongAnswer() {
-
+function nextQuestion() {
+  //needs to dynamically create buttons
+  //adds hidden class and removes for next question
+  for (let i = 0; i < questions.length; i++) {
+    $("#quiz-question").html(questions[i].question);
+    // $(questions[0].question[0]).appendTo("#quiz-questions")
+    $("#btn1").html(questions[i].answerOptions[0]);
+    $("#btn2").html(questions[i].answerOptions[1]);
+    $("#btn3").html(questions[i].answerOptions[2]);
+    $("#btn4").html(questions[i].answerOptions[3]);
+  }
+}
+//on button click check for answer.
 // }
 // // WHEN all questions are answered or the timer reaches 0
 // // THEN the game is over
-
-// function gameOver() {
-//   if(secondsLeft === 0 logScore())
-// } 
 // // WHEN the game is over
 // // THEN I can save my initials and my score
-
-
-// function startQuiz () {
-
-// }
-
-
-// function answerSelected() {
-
-  // }
+// function gameOver() {
+//function logScore() {
+//   userInitials saved to local storage followed by startQuiz function
+// // WHEN I answer a question incorrectly
+// // THEN time is subtracted from the clock
+// function wrongAnswer() {
+function answerSelected(event) {
+  let answer = event.target.textContent;
+  if (secondsLeft === 0 || questions.length > 3) {
+    gameOver();
+  }
+  if (answer === questions[questionIndex].correctAnswer) {
+    score += 100;
+  } else {
+    secondsLeft -= 15;
+  }
+}
+nextQuestion();
